db45b1c75ef418f3d242710c23d2d44c8d6dd3c2
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2022-01-06 11:46:32 -0800

    use argparse to validate arguments

--- HexUploader.py
+++ HexUploader.py
@@ -2,67 +2,62 @@
 # Args: <fulldevicename> e.g /dev/ttyUSB0 aka the port connecting the PICDuino
 
 import sys
+import argparse
 import serial.tools.list_ports
 
 # Pass in command to print error message
-def validate_and_open_port():
-    if len(sys.argv) < 3:
-        # A port wasn't provided
-        print(f"Usage: python3 receiver.py {sys.argv[1]} <portname>")
-        exit()
-
-    if sys.argv[1] not in ports:
+def validate_and_open_port(port, ports):
+    if port not in ports:
         # The port can't be found
-        print(f"Error: Invalid port name: \"{sys.argv[1]}\"")
+        print(f"Error: Invalid port name: \"{port.name}\"")
         exit()
 
     # Open port
     return serial.Serial(sys.argv[1])
 
 if __name__ == "__main__":
-    # Input validation
-    if len(sys.argv) < 2:
-        # A command wasn't provided
-        print("Usage: python3 receiver.py <command>")
-        print("Available commands:")
-        print("listports")
-        print("upload")
-        print("read")
-        print("send")
-        exit()
+    # Specify required/optional args
+    parser = argparse.ArgumentParser(description='Command line toolkit for the PICDuino')
+    # List of commands
+    parser.add_argument('command', choices=[
+        'listports',
+        'upload',
+        'read',
+        'send'
+    ], help="what you want to do with the PICDuino")
+    
+    # Only require these arguments for specific commands
+    parser.add_argument('-p', dest="port", help="the port name connecting the PICDuino", required=
+            ('upload' in sys.argv or 'read' in sys.argv or 'send' in sys.argv)
+            )
+    parser.add_argument('-m', dest="message", help="message to send to PICDuino", required='send' in sys.argv)
+    parser.add_argument('-f', dest="file", help="the name of the hex binary to upload", required='upload' in sys.argv)
+
+    # Get user inputted commands
+    args = parser.parse_args()
 
     # Get available ports
     ports = serial.tools.list_ports.comports()
    
     # Process command
-    if sys.argv[1] == "listports":
+    if args.command == "listports":
         # Print all the port names
+        print("Ports found:", len(ports))
         for port in ports:
             print(port.name)
-    elif sys.argv[1] == "upload":
-        # TODO
-        pass
-    elif sys.argv[1] == "read":
-        port = validate_and_open_port()
-        
-        # Print output
-        print("Press Ctrl+C to close the program. Receiver output:")
-        while(True):
-            # read_until() continues to read until it encounters \n
-            # Don't print newline since string already ends with \n
-            # Convert byte array to string
-            print(port.read_until().decode(), end="")
-        # TODO: need to properly terminate
-    elif sys.argv[1] == "send":
-        port = validate_and_open_port()
-
-        if len(sys.argv) < 4:
-            # No message found
-            print("Usage: python3 receiver.py send <portname> <message>")
-            exit()
-
-        # Send message
-        port.write(bytes(sys.argv))
-
-    port.close()
+    elif args.command in ['read', 'send', 'upload']:
+        port = validate_and_open_port(args.port, ports)
+        if args.command == 'read':
+            # Print output
+            print("Press Ctrl+C to close the program. Receiver output:")
+            while(True):
+                # Don't print newline since string already ends with \n
+                print(port.read_until().decode(), end="")
+            # TODO: need to properly terminate
+        elif command == "send":
+            # Send message
+            port.write(bytes(message))
+        elif command == "upload":
+            # TODO
+            pass
 
