f76d1f2f0f296ff26a3c3230e463e99080d75ac2
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2022-01-05 20:47:38 -0800

    add input validation and gui code

--- HexUploader.py
+++ HexUploader.py
@@ -1,12 +1,31 @@
 import serial.tools.list_ports
 
+# UI libraries
+from tkinter import *
+from tkinter import ttk
+
 if __name__ == "__main__":
+    
+    '''
+    # GUI
+    root = Tk()
+    #ttk.Button(root, text="Upload").grid()
+    Text(root, width=40, height=40).grid()
+    root.mainloop()
+    '''
+
+    # '''
     selection = -1
     valid_selection = False
     while valid_selection == False:
         # Print available serial ports
-        print("List of serial ports:")
         ports = serial.tools.list_ports.comports()
+
+        if len(ports) == 0:
+            print("There are no ports available. Please reconnect your PICDuino and try again")
+            exit()
+
+        print("List of serial ports:")
         for i in range(0, len(ports)):
             port = ports[i]
             print(f"{i}: {port[0]}")
@@ -20,11 +39,12 @@ if __name__ == "__main__":
         else:
             valid_selection = True
 
-    # Open port
+    # Open and read from port
+    print("Press Ctrl+C to close the program. Receiver output:")
     port = serial.Serial(port[0])
-    
     while(True):
         print(port.read_until())
 
     port.close()
+    # '''
 
