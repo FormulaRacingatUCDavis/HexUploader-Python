bba46930ed4c764a9eb02c660727c2ce9bb63fa0
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2022-01-05 23:19:12 -0800

    pass in port name as arg instead

--- receiver.py
+++ receiver.py
@@ -1,41 +1,31 @@
 # Script that receives serial input from the PICDuino
+# Args: <fulldevicename> e.g /dev/ttyUSB0 aka the port connecting the PICDuino
+
+import sys
 import serial.tools.list_ports
 
 if __name__ == "__main__":
-    selection = -1
-    valid_selection = False
-    while valid_selection == False:
-        # Print available serial ports
-        ports = serial.tools.list_ports.comports()
+    # Input validation
 
-        if len(ports) == 0:
-            print("There are no ports available. Please reconnect your PICDuino and try again")
-            exit()
+    if len(sys.argv) < 2:
+        print("Usage: python3 HexUploader.py <fulldevicename>")
+        exit()
 
-        print("<selection number>: <serial port>")
-        for i in range(0, len(ports)):
-            port = ports[i]
-            print(f"{i}: {port[0]}")
-
-        # Prompt user to open port
-        selection = int(input("Enter selection number for port to read from:"))
-
-        # Input validation
-        if selection < 0 or selection >= len(ports):
-            print("Error: please enter a valid selection!")
-        else:
-            valid_selection = True
+    ports = serial.tools.list_ports.comports()
+    if sys.argv[1] not in ports:
+        print(f"Error: Invalid port name: \"{sys.argv[1]}\"")
+        exit()
 
     # Open and read from port
     print("Press Ctrl+C to close the program. Receiver output:")
-    port = serial.Serial(port[0])
+    port = serial.Serial(sys.argv[1])
     while(True):
         # Decode byte array to string
         # https://docs.python.org/3/library/stdtypes.html#bytes.decode
         # Don't print newline since string already ends with \n
-        # read_until() continues to read until it encounters a newline
+        # read_until() continues to read until it encounters \n
         print(port.read_until().decode(), end="")
 
-    # TODO: this code can never run
+    # TODO: this code will never run
     port.close()
 
