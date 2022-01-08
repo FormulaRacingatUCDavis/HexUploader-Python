51817e08106c59425c615119e912735add7319b4
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2022-01-08 09:06:44 -0800

    refactor parser code

--- HexUploader.py
+++ HexUploader.py
@@ -3,17 +3,14 @@ import argparse
 import serial
 from serial.tools.list_ports import comports
 
-def read_port(device_path):
-    # Receive and print microcontroller output
-    print("Press Ctrl+C to close the program. Receiver output:")
-    # Open connection to port
-    with serial.Serial(device_path) as port:
-        while(True):
-            # Print string already ends with \n
-            print(port.read_until().decode(encoding="ascii"), end="")
+# Signals
 
-if __name__ == "__main__":
-    # Parse subcommands and their required args
+# Send this to start bootloader
+BOOTLOADER_REQUEST = 0x55
+# Expected response from microcontroller after it starts bootloader
+BOOTLOADER_RESPONSE = 0x69
+
+def create_parser():
     parser = argparse.ArgumentParser(description='Command line toolkit for the PICDuino microcontroller')
     # Create subparsers for each subcommand
     # Each subparser handles the required args for a subcommand
@@ -24,7 +21,6 @@ if __name__ == "__main__":
     parser_read = subparsers.add_parser('read', help='read data from microcontroller')
 
     # Required subcommand args
-
     # Port required for all operations that connect to the microcontroller
     PORT_FLAG = '-p'
     PORT_DESTNAME = 'device_path'
@@ -35,6 +31,19 @@ if __name__ == "__main__":
     parser_upload.add_argument('-f', dest="file", help="Name of the hex binary to upload", required=True)
     # Add option to read immediately after uploading
     parser_upload.add_argument('--read', dest="read_after_upload", action="store_true", help="read immediately after uploading")
+
+def read_port(device_path):
+    # Receive and print microcontroller output
+    print("Press Ctrl+C to close the program. Receiver output:")
+    # Open connection to port
+    with serial.Serial(device_path) as port:
+        while(True):
+            # Print string already ends with \n
+            print(port.read_until().decode(encoding="ascii"), end="")
+
+if __name__ == "__main__":
+    # Parse subcommands and arguments
+    parser = create_parser()
 
     # Validate and get subcommand + flags
     args = parser.parse_args()
@@ -60,6 +69,7 @@ if __name__ == "__main__":
         if args.subcommand == 'read':
             read_port(args.device_path)
         elif args.subcommand == "upload":
+            # Open hex file
             try:
                 hexfile = open(args.file)
             except:
