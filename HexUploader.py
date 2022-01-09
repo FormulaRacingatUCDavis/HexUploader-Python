2630a7d216ee975d6f7c99b30c098da08992053e
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2022-01-09 00:27:01 -0800

    add verify subcommand to perform handshake

--- HexUploader.py
+++ HexUploader.py
@@ -1,14 +1,17 @@
 import sys
+import time
+
 import argparse
 import serial
 from serial.tools.list_ports import comports
 
-# Signals
+# PIC 16 Signals
 
-# Send this to start bootloader
-BOOTLOADER_REQUEST = 0x55
-# Expected response from microcontroller after it starts bootloader
-BOOTLOADER_RESPONSE = 0x69
+PIC16_ESC_BYTE = "05"
+# Verifies if we are connected to a PICDuino board
+PICDUINO_HANDSHAKE = PIC16_ESC_BYTE + "AA"
+# Expected response
+PICDUINO_HANDSHAKE_RESPONSE = "99"
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Command line toolkit for the PICDuino microcontroller')
@@ -19,6 +22,7 @@ def create_parser():
     parser_listports = subparsers.add_parser('listports', help='lists all the serial port paths')
     parser_upload = subparsers.add_parser('upload', help='upload a hex binary file to the microcontroller')
     parser_read = subparsers.add_parser('read', help='read data from microcontroller')
+    parser_verify = subparsers.add_parser('verify', help='checks if microcontroller is a PICDuino')
 
     # Required subcommand args
     # Port required for all operations that connect to the microcontroller
@@ -27,6 +31,7 @@ def create_parser():
     PORT_HELP_STR = 'device path for port connecting the microcontroller'
     parser_upload.add_argument(PORT_FLAG, dest=PORT_DESTNAME, help=PORT_HELP_STR, required=True)
     parser_read.add_argument(PORT_FLAG, dest=PORT_DESTNAME, help=PORT_HELP_STR, required=True)
+    parser_verify.add_argument(PORT_FLAG, dest=PORT_DESTNAME, help=PORT_HELP_STR, required=True)
     # File required to upload to microcontroller
     parser_upload.add_argument('-f', dest="file", help="Name of the hex binary to upload", required=True)
     # Add option to read immediately after uploading
@@ -59,7 +64,7 @@ if __name__ == "__main__":
         print("Ports found:", len(ports))
         for port in ports:
             print(port.device)
-    elif args.subcommand in ['upload', 'read']:
+    elif args.subcommand in ['upload', 'read', 'verify']:
         # These commands require a port connection
         # Did the user enter a valid port?
         device_paths = map(lambda port: port.device, ports)
@@ -70,6 +75,27 @@ if __name__ == "__main__":
 
         if args.subcommand == 'read':
             read_port(args.device_path)
+        elif args.subcommand == 'verify':
+            with serial.Serial(args.device_path) as port:
+                # Perform handshake
+                handshake_request = bytes.fromhex(PICDUINO_HANDSHAKE)
+                port.write(handshake_request)
+
+                print("Performing handshake with microcontroller...")
+
+                start = time.time()
+                while 1:
+                    if time.time() - start >= 3: # 3 seconds until timeout
+                        print("Cannot verify if board is a PICDuino")
+                        exit()
+                    byte = port.read()
+                    # Issue: the 0x99 can come from anywhere, not just the PIC16
+                    if byte.hex() == PICDUINO_HANDSHAKE_RESPONSE:
+                        # Verified that microcontroller is PICDuino
+                        break
+                    # Otherwise keep listening
+
+                print("This board is a PICDuino")
         elif args.subcommand == "upload":
             # Open hex file
             try:
